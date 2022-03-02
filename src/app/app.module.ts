e3367f56f4d547197224c024a01ba082a1b9554f
Eric Trasancos <erictrasancos94@gmail.com>
2022-03-02 17:04:03 -0300

    Uso de github pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ContactComponent } from './components/contact/contact.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule
